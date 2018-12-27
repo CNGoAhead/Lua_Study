4b2a0a6463c9d95a3bbe99bdb7b1d35aaad0c59b
WHS <894696854@qq.com>
2018-12-27 18:32:24 +0800

    fix:新增As Is接口

---
+++ Base/DataType/AsAndIs.lua
@@ -0,0 +1,30 @@
+function Is(obj, className)
+    if obj.__class__ and obj.__super__ then
+        obj.__is_map__ = obj.__is_map__ or {}
+
+        if obj.__is_map__[className] ~= nil then
+            return obj.__is_map__[className] ~= false
+        end
+
+        if obj.__class__ == className then
+            obj.__is_map__[className] = obj
+            return true
+        else
+            for _, v in ipairs(obj.__super__) do
+                if Is(v, className) then
+                    return true
+                end
+            end
+        end
+
+        obj.__is_map__[className] = false
+    end
+    return false
+end
+
+function As(obj, className)
+    if not obj.__is_map__ or obj.__is_map__[className] == nil then
+        Is(obj, className)
+    end
+    return obj.__is_map__[className] or nil
+end
